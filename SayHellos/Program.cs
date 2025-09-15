ec1b8c41ec11f43e82d0bb43f0baaa6ee5d8bf2f
charlie <Charliesamuel83@gmail.com>
2025-09-15 11:05:54 +0200

    Add project files.

---
+++ SayHellos/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace SayHellos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Vad heter du?");
+            string name = Console.ReadLine();
+            SayHello(name);
+
+        }
+        static void SayHello(string name)
+        {
+            Console.WriteLine($"Hello, {name}");
+        }
+    }
+    }
+}
