34a5bb41739cf9fd8ec4252270fd0f07ff5f0519
charlie <Charliesamuel83@gmail.com>
2025-09-16 13:28:52 +0200

    Lade till kommentarer

--- SayHellos/Program.cs
+++ SayHellos/Program.cs
@@ -6,13 +6,20 @@
         {
             Console.WriteLine("Vad heter du?");
             string name = Console.ReadLine();
-            SayHello(name);
+            while (string.IsNullOrWhiteSpace(name))  // loop för att säkerställa att användaren skriver in ett namn
 
+            {
+                Console.WriteLine("Du måste skriva in ett namn!");
+                Console.WriteLine("Vad heter du?");
+                name = Console.ReadLine();
+            }
+
+            SayHello(name);
         }
-        static void SayHello(string name)
+        static void SayHello(string name) //    metod med parameter
         {
             Console.WriteLine($"Hello, {name}");
         }
-    }
+    
     }
 }
